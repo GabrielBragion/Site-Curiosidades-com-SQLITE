aedae11167b30d5acc994911304eabb2185b27fe
Gabriel Bragion <gabriel.bragion.micco@gmail.com>
2024-10-17 15:25:57 +0100

    correções no arquivo server

--- server/authRoutes.js
+++ server/authRoutes.js
@@ -3,30 +3,22 @@ const path = require("path");
 const session = require("express-session");
 const router = express.Router();
 require("dotenv").config(); // Carrega as variáveis de ambiente do arquivo .env
-const bcrypt = require("bcrypt");
 
-// A senha deve ser definida como uma variável de ambiente no Railway
-const CORRECT_PASSWORD_HASH = bcrypt.hashSync(process.env.ADMIN_PASSWORD, 10);
+// Definindo a senha correta diretamente
+const CORRECT_PASSWORD = process.env.ADMIN_PASSWORD; // Certifique-se de que esta variável está definida no Railway
 
 // Processando o envio do formulário de login
 router.post("/login", (req, res) => {
   const password = req.body.password;
 
-  // Comparar a senha inserida com o hash da senha correta
-  bcrypt.compare(password, CORRECT_PASSWORD_HASH, (err, result) => {
-    if (err) {
-      console.error("Erro ao comparar senhas:", err);
-      return res.status(500).send("Erro interno do servidor");
-    }
-
-    if (result) {
-      // Se a senha estiver correta
-      req.session.loggedIn = true;
-      res.redirect("/adicionar");
-    } else {
-      res.send('Senha incorreta! <a href="/login">Tente novamente</a>');
-    }
-  });
+  // Verificar se a senha inserida está correta
+  if (password === CORRECT_PASSWORD) {
+    // Se a senha estiver correta
+    req.session.loggedIn = true;
+    res.redirect("/adicionar");
+  } else {
+    res.send('Senha incorreta! <a href="/login">Tente novamente</a>');
+  }
 });
 
 // Middleware para proteger rotas privadas
